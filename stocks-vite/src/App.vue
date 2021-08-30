7aebce73febe162e1ad995a6b1d537f8bc1c0a02
uzunvictor <v3i3c3@gmail.com>
2021-08-30 12:39:34 +0300

    start of stock.vue

---
+++ stocks-vite/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Stock from './components/Stock.vue'
+</script>
+
+<template>
+<div class="container">
+  <div class="title">
+    <h2>Stock Page</h2>
+  </div>
+</div>
+</template>
+
