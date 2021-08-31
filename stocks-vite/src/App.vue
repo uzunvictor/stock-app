19843ffc2ebcbcba43ca1ba386279e50267da742
uzunvictor <v3i3c3@gmail.com>
2021-08-31 10:56:44 +0300

    create the main.css

--- stocks-vite/src/App.vue
+++ stocks-vite/src/App.vue
@@ -1,13 +1,20 @@
-<script setup lang="ts">
+<script lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { defineComponent } from '@vue/runtime-core'
 import Stock from './components/Stock.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { Stock}
+})
 </script>
 
 <template>
 <div class="container">
   <div class="title">
     <h2>Stock Page</h2>
+    <Stock />
   </div>
 </div>
 </template>
--- stocks-vite/src/components/Stock.vue
+++ stocks-vite/src/components/Stock.vue
@@ -1,9 +1,52 @@
-<script setup lang="ts">
+<script lang="ts">
+import axios from 'axios';
 
+export default {
+  name: 'Stocks',
+  data() {
+    return {
+      stock: [],
+      errors: [],
+      selected: '',
+    };
+  },
+
+  created() {
+    axios
+      .get(
+        'https://financialmodelingprep.com/api/v3/profile/AAPL,NVDA,TSLA,AMD,INTC,MDB,SPCE,V,DAL,DOCU,OKTA,AMZN,PINS,TRIP,GDDY,DIS,MCD,NOK,UPWK,IBM,FB,ZM,OZON,NFLX,EA,HLT,H,CCL?apikey=f9b624109eac48ba5e6ad8bc5cb106b8',
+      )
+      .then((response) => {
+        this.stock = response.data;
+        console.log(response.data);
+      })
+      .catch((e) => {
+        this.errors.push = e;
+      });
+  },
+};
 </script>
 
 <template>
- 
-
+  <div class="stock">
+    <div class="stock-item" v-for="item in stock" :key="item.phone">
+      <div class="stock-item__info">
+        <div class="stock-item__cover">
+          <img :src="item.image" alt="" />
+        </div>
+        <h3 class="stock-item__title">{{ item.companyName }}</h3>
+      </div>
+      <span class="stock-item__price">{{ item.price }} $</span>
+    </div>
+    <h3>Get info</h3>
+    <select v-model="selected">
+      <option valule="" disabled>Select option</option>
+      <option v-for="value in stock" :value="value.description" :key="value.phone">
+        {{ value.companyName }}
+      </option>
+    </select>
+    <div class="info">
+      {{ selected }}
+    </div>
+  </div>
 </template>
-
